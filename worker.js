8f7784f7a0533b8a9856f2f2cf44db21cde6aa63
Nathan Clevenger <nateclev@gmail.com>
2022-09-16 13:46:12 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,12 @@ export default {
   fetch: async (req, ctx) => {
     const data = fetch('https://unicode.org/Public/UNIDATA/UnicodeData.txt').then(res => res.json())
     const rows = data.split('\n')
-    const [ value, nam, category, className, bidirectionalCategory, mapping, decimalDigitValue, digitValue, 
-            numericValue, mirrored, unicodeName, comment, uppercaseMapping, lowercaseMapping, titlecaseMapping] = rows.split(';')
-    
+    const categories = rows.reduce((acc, row) => {
+      const [ value, nam, category, className, bidirectionalCategory, mapping, decimalDigitValue, digitValue, 
+              numericValue, mirrored, unicodeName, comment, uppercaseMapping, lowercaseMapping, titlecaseMapping] = row.split(';')
+      const symbol = decodeURIComponent('%' + value)
+      return {...acc, [category]: { symbol, value, nam, category, className, bidirectionalCategory, mapping, decimalDigitValue, digitValue, 
+                                    numericValue, mirrored, unicodeName, comment, uppercaseMapping, lowercaseMapping, titlecaseMapping }}
+    }, {})
+    return new Response(JSON.stringify({categories}, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
 }
