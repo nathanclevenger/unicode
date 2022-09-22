f2f2cdb9b5ca2d130946d72247153f9e0f7de902
Nathan Clevenger <nateclev@gmail.com>
2022-09-22 04:33:37 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,7 @@
 export default {
-  fetch: async (req, ctx) => {
-    const data = fetch('https://unicode.org/Public/UNIDATA/UnicodeData.txt').then(res => res.text())
-    const rows = data.split('\n')
-    const categories = rows.reduce((acc, row) => {
-      const [ value, nam, category, className, bidirectionalCategory, mapping, decimalDigitValue, digitValue, 
-              numericValue, mirrored, unicodeName, comment, uppercaseMapping, lowercaseMapping, titlecaseMapping] = row.split(';')
-      const symbol = decodeURIComponent('%' + value)
-      return {...acc, [category]: { symbol, value, nam, category, className, bidirectionalCategory, mapping, decimalDigitValue, digitValue, 
-                                    numericValue, mirrored, unicodeName, comment, uppercaseMapping, lowercaseMapping, titlecaseMapping }}
-    }, {})
-    return new Response(JSON.stringify({categories}, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+  fetch: async (req, env) => {
+    const { data } = env.CSV.fetch('https://csv.do/delimiter=;&fields=value,name,category,className,bidirectionalCategory,mapping,decimalDigitValue,digitValue,numericValue,mirrored,unicodeName,comment,uppercaseMapping,lowercaseMapping,titlecaseMapping/unicode.org/Public/UNIDATA/UnicodeData.txt').then(res => res.json())
+    const symbols = data.map(row => ({symbol: String.fromCharCode(parseInt(row.value,16)),...row}))
+    return new Response(JSON.stringify({symbols}, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
   }
 }
